05ba2bc83bb0e3172ac65a3a3e06b6bffd6134dc
Israel Monjes <israelmonjesreyes@gmail.com>
2023-10-23 17:54:25 -0600

    Actualizacion de comentarios

--- app/main.py
+++ app/main.py
@@ -2,43 +2,43 @@ import fitz  # PyMuPDF
 import os
 
 def insert_signature(pdf_path, image_path, output_path, coordinates, scale_factor=1.0):
-    # Load the PDF
+    # Carga el PDF
     pdf_document = fitz.open(pdf_path)
 
-    # Get the third page and its dimensions
+    # Obtiene la página y sus dimensiones que deseas firmar
     page = pdf_document.load_page(2)
     page_width = page.rect.width
     page_height = page.rect.height
 
-    # Calculate the position to insert the image
+    # Calcula la posición para insertar la imagen
     x, y = coordinates
     x = x * page_width
     y = (1 - y) * page_height
 
-    # Load the image
+    # Carga la imagen a insertar
     image = fitz.open(image_path)
     image_page = image.load_page(0)
 
-    # Get the original image dimensions
+    # Obtiene las dimensiones originales de la imagen
     image_width = image_page.rect.width
     image_height = image_page.rect.height
 
-    # Calculate the new image dimensions using the scale_factor
+    # Calcula las nuevas dimensiones de la imagen utilizando el factor de escala (scale_factor)
     new_image_width = image_width * scale_factor
     new_image_height = image_height * scale_factor
 
-    # Set the position of the image
+    # Establece la posición de la imagen
     image_rect = fitz.Rect(x, y, x + new_image_width, y + new_image_height)
 
-    # Overlay the image on the page with the new dimensions
+    # Sobrepone la imagen en la página con las nuevas dimensiones
     page.insert_image(image_rect, pixmap=image_page.get_pixmap(), overlay=True)
 
-    # Save the modified PDF with the inserted image
+    # Guarda el pdf con las imagenes insertadas
     pdf_document.save(output_path)
     pdf_document.close()
 
 def process_files_in_folder(input_folder, output_folder, image_path, scale_factor=1.0):
-    # Obtén la lista de archivos en la carpeta de entrada
+    # Obtiene la lista de archivos en la carpeta de entrada
     files = [f for f in os.listdir(input_folder) if f.endswith('.pdf')]
 
     for file_name in files:
@@ -55,9 +55,9 @@ def process_files_in_folder(input_folder, output_folder, image_path, scale_facto
         insert_signature(input_file_path, image_path, output_file_path, coordinates, scale_factor)
 
 # Rutas de las carpetas y la imagen
-input_folder = 'C:\\Users\\israe\\OneDrive\\Desktop\\Contratos'  # Carpeta de entrada
-output_folder = 'C:\\Users\\israe\\OneDrive\\Desktop\\Modificados'  # Carpeta de salida
-image_path = 'C:\\Users\\israe\\Downloads\\firma.png'  # Ruta de la imagen de la firma
+input_folder = 'tu ruta de archivos a modificar'  # Carpeta de entrada
+output_folder = 'tu ruta donde iran a parar tus archivos modificados'  # Carpeta de salida
+image_path = 'tu ruta de la imagen a insertar'  # Ruta de la imagen de la firma
 
 # Escala para el tamaño de la imagen (ajusta según sea necesario)
 scale_factor = 0.35
